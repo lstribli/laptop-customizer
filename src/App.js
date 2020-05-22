7673e3c54f8954d5116ee8e246dabf2c828da7d1
Logan Stribling <logan.stribling@gmail.com>
2020-05-21 22:42:19 -0600

    why?

--- src/App.js
+++ src/App.js
@@ -7,6 +7,48 @@ import Cart from './cart';
 
 export default class App extends React.Component {
   state = {
+    features: {
+      Processor: [
+        {
+          name: '17th Generation Intel Core HB (7 Core with donut spare)',
+          cost: 700
+        },
+        {
+          name: 'Professor X AMD Fire Breather with sidewinder technology',
+          cost: 1200
+        }
+      ],
+      "Operating System": [
+        {
+          name: 'Ubuntu Linux 16.04',
+          cost: 200
+        },
+        {
+          name: 'Bodhi Linux',
+          cost: 300
+        }
+      ],
+      "Video Card": [
+        {
+          name: 'Toyota Corolla 1.5v',
+          cost: 1150.98
+        },
+        {
+          name: 'Mind mild breeze 2000',
+          cost: 1345
+        }
+      ],
+      Display: [
+        {
+          name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
+          cost: 1500
+        },
+        {
+          name: '15.3" HGTV (3840 x 2160) Home makeover edition',
+          cost: 1400
+        },
+      ]
+    },
     selected: {
       Processor: {
         name: '17th Generation Intel Core HB (7 Core with donut spare)',
@@ -36,8 +78,8 @@ export default class App extends React.Component {
   };
 
   render() {
-
-    const { features } = this.props;
+    console.log(this.state)
+    const { features } = this.state;
     const { selected } = this.state;
 
     return (
